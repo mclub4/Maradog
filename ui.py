82d88a5b380bf3ca3a252ce6253fbe19e2be3294
JihunChoi <geehoon62017@gmail.com>
2019-11-22 15:32:21 +0900

    modified class structure but we need to modify about class structure and file structure

--- ui.py
+++ ui.py
@@ -4,6 +4,25 @@ from PyQt5.QtCore import *
 import sys
 
 gif_file = "avatar_walk1_default.gif"
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+
+    def init_ui(self):
+        self.resize(1046, 3772)  # original 1046 x 3772
+        self.setWindowTitle("Forest of Patience")
+
+        # background
+        background_image = QImage('back_ground_1.png')
+        modified_background_image = background_image.scaled(QSize(1046, 3772))  # original 1046 x 3772
+        palette = QPalette()
+        palette.setBrush(10, QBrush(modified_background_image))
+        self.setPalette(palette)
+
+        # create main layout
+        main_layout = QVBoxLayout()
+        self.setLayout(main_layout)
+
 class GifPlayer(QWidget):
     def __init__(self, title, gif_file, parent=None):
         QWidget.__init__(self, parent)
