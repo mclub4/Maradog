6e6c1ed0333e2d4160396c48874cf2a526005d4d
JihunChoi <geehoon62017@gmail.com>
2019-12-03 14:15:20 +0900

    modify

--- initial.py
+++ initial.py
@@ -1,11 +1,5 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
 from block import Block
-import sys
-import time
 from character import *
-
 
 
 class Main(QWidget):
