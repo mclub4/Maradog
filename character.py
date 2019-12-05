aa504c71c74d7934cc737f4d8e0ae64f29f47783
JihunChoi <geehoon62017@gmail.com>
2019-12-05 14:56:56 +0900

    modify plan

--- character.py
+++ character.py
@@ -1,10 +1,6 @@
-# postpone
-# i will come back when i have time to fix it
-
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from PyQt5 import QtCore
 import sys
 
 
@@ -14,28 +10,35 @@ class Character(QLabel):
     is_first_left = True
     is_first_release_right = True
     is_first_release_left = True
-    current_x = 0
-    current_y = 0
-
+    current_x = -832  # -832
+    current_y = 327
 
     def __init__(self):
         super().__init__()
+        self.setVisible(False)
         self.movie = QMovie('resource/avatar_stand1_default.gif', QByteArray(), self)
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.setAlignment(Qt.AlignCenter)
         self.movie.setCacheMode(QMovie.CacheAll)
+        self.move(10, 100)
         self.setMovie(self.movie, self.current_x, self.current_y)
         self.movie.start()
         self.movie.loopCount()
+        self.move(self.current_x + 1, self.current_y)
+        self.setVisible(True)
+        self.move(self.current_x - 1, self.current_y)
+
 
 
     def setMovie(self, QMovie, x, y):
         super().setMovie(QMovie)
         self.move(x, y)
 
-
     def keyPressEvent(self, e):
         self.move(self.current_x, self.current_y)
+        print(self.current_x, self.current_y)
+        print("위는 커렌트 아래는 현재")
+        print(self.pos().x(), self.pos().y())
         # right
         if e.key() == Qt.Key_D:
             if self.is_first_right:
@@ -54,10 +57,9 @@ class Character(QLabel):
             # self.character.move(self.current_x + 5, self.current_y)
             self.move(self.current_x + 1, self.current_y)
             self.setVisible(True)
-            self.move(self.current_x + 4, self.current_y)
+            self.move(self.current_x + 6, self.current_y)
             self.current_x = self.pos().x()
             self.current_y = self.pos().y()
-
 
         # left
         elif e.key() == Qt.Key_A:
@@ -76,23 +78,9 @@ class Character(QLabel):
             # self.character.move(self.current_x + 5, self.current_y)
             self.move(self.current_x - 1, self.current_y)
             self.setVisible(True)
-            self.move(self.current_x - 4, self.current_y)
+            self.move(self.current_x - 6, self.current_y)
             self.current_x = self.pos().x()
             self.current_y = self.pos().y()
-
-
-        # up
-        elif e.key() == Qt.Key_W:
-            self.move(self.current_x, self.current_y - 5)
-            self.current_x = self.pos().x()
-            self.current_y = self.pos().y()
-
-        # down
-        elif e.key() == Qt.Key_S:
-            self.move(self.current_x, self.current_y + 5)
-            self.current_x = self.pos().x()
-            self.current_y = self.pos().y()
-
 
     def keyReleaseEvent(self, event):
         key = event.key()
@@ -135,7 +123,6 @@ class Character(QLabel):
                 self.current_y = self.pos().y()
                 self.is_first_release_left = False
                 self.is_first_left = True
-
 
 
 if __name__ == '__main__':
--- initial.py
+++ initial.py
@@ -7,7 +7,7 @@ class Main(QWidget):
         QWidget.__init__(self, parent)
 
         # initial mainWindow
-        self.resize(1046, 3772) # original 1046 x 3772
+        self.resize(1440, 627)  # original 1046 x 3772
         self.setWindowTitle("Forest of Patience")
 
         # create main layout
@@ -15,23 +15,29 @@ class Main(QWidget):
         self.setLayout(main_layout)
 
         # background
-        background_image = QImage('resource/back_ground_1.png')
-        modified_background_image = background_image.scaled(QSize(1046, 3772)) # original 1046 x 3772
+        background_image = QImage('resource/back_ground_4.png')
+        modified_background_image = background_image.scaled(QSize(2000, 1080))  # original 3326 X 880
         palette = QPalette()
         palette.setBrush(10, QBrush(modified_background_image))
         self.setPalette(palette)
 
-        # blocks
-        block = Block(20000, 1000)
-        main_layout.addWidget(block)
+        # # blocks
+        # block = Block(20000, 1000)
+        # main_layout.addWidget(block)
 
         # character
         self.character = Character()
+        self.character.setVisible(False)
+        # self.character.move(100, 1000)
+        # print(self.character.pos().x(), self.character.pos().y())
         main_layout.addWidget(self.character)
 
 
     def keyPressEvent(self, event):
         self.character.keyPressEvent(event)
+
+        if event.key() == Qt.Key_Escape:
+            self.close()
 
     def keyReleaseEvent(self, event):
         self.character.keyReleaseEvent(event)
@@ -40,5 +46,5 @@ class Main(QWidget):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main = Main()
-    main.show()
+    main.showFullScreen()
     sys.exit(app.exec_())
